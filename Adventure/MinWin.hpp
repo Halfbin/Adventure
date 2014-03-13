156f8e4a73b4ffb82c7045b9d9ae7dd0f3899202
Ben Peggs <b.peggs@googlemail.com>
2014-03-13 00:00:59 +0000

    Improved MinWin.hpp.

--- Adventure/MinWin.hpp
+++ Adventure/MinWin.hpp
@@ -1,25 +1,26 @@
 #pragma once
 
 #ifdef  _MBCS
-#   error "_MBCS (multi-byte character set) is defined, but only Unicode is supported"
+# error "Do not use _MBCS builds. Use _UNICODE instead."
 #endif
-#undef  _UNICODE
-#define _UNICODE        // For [tchar.h]
-#undef  UNICODE
-#define UNICODE         // For [windows.h]
 
-#undef  NOMINMAX
-#define NOMINMAX        // C++ standard library compatibility
-#undef  STRICT
-#define STRICT          // C++ type-checking compatibility
+#pragma warning (push)
+#pragma warning (disable: 4005)
+
+#define _UNICODE // tchar.h
+#define UNICODE  // windows.h
+#define NOMINMAX // ugh
+#define STRICT   // just in case
+#define WIN32_LEAN_AND_MEAN
 
 #ifndef _WIN32_WINNT
-#   define _WIN32_WINNT     _WIN32_WINNT_WINXP  // Minimum version as per 2011/2012.
-#endif
-#ifdef  _MSC_VER            // Visual C++ only (the SDK is Visual C++ specific).
-#   include <SDKDDKVer.h>   // E.g. g++ 4.4.1 does not support this file/scheme.
+# define _WIN32_WINNT _WIN32_WINNT_WIN7
 #endif
 
-#undef  WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
+#pragma warning (pop)
+
+#ifdef  _MSC_VER
+# include <SDKDDKVer.h>
+#endif
+
 #include <Windows.h>
